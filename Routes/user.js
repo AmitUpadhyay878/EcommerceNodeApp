ca2bfccfda77ed76549809e5e19d4cc99b52741d
Pratik Chauhan <pratikc.multiqos@gmail.com>
2022-06-14 16:59:10 +0530

    noChanges

--- Routes/user.js
+++ Routes/user.js
@@ -1,61 +1,98 @@
-const {verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin} = require("./verifyToken")
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require("./verifyToken");
 
-const router = require("express").Router()
+const router = require("express").Router();
 const Bcrypt = require("bcrypt");
 const User = require("../Models/User");
 
 // Mothod for Update
-router.put("/:id",verifyTokenAndAuthorization,async(req,res)=>{
-    const encPass = Bcrypt.hashSync(req.body.password, 12);
-    if(req.body.password){
-          req.body.password= encPass
-     }
-   try{
-        const updatedUser= await User.findByIdAndUpdate(req,params.id,{
-            $set:req.body
-        },{new:true})
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  //   const encPass = Bcrypt.hashSync(req.body.password, 12);
+  //   if (req.body.password) {
+  //     req.body.password = encPass;
+  //   }
+  try {
+   
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
 
-        res.status(200).json(updatedUser)
-
-   }catch(err){
-            res.status(500).json(err)
-   }
-})
+    res.status(200).json(updatedUser);
+  } catch (err) {
+   
+    res.status(500).json(err);
+  }
+});
 
 //Method For Delete
-router.delete("/:id",verifyTokenAndAuthorization,async(req,res)=>{
-            try{
-                    await User.findByIdAndDelete(req.param.id)
-                    res.status(200).json("User has Deleted")
-
-            }catch(err){
-                res.status(500).json(err)
-            }
-})
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.param.id);
+    res.status(200).json("User has Deleted");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //Method For Select  User By ID
-router.get("/find/:id",verifyTokenAndAdmin,async(req,res)=>{
-    try{
-           const user =  await User.findById(req.param.id)
-          
-           const {decPass,...others} = user._doc
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.param.id);
 
-           res.status(201).json(others);
+    const { decPass, ...others } = user._doc;
 
-    }catch(err){
-        res.status(500).json(err)
-    }
-})
+    res.status(201).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 //Method For Select All User
-router.get("/",verifyTokenAndAdmin,async(req,res)=>{
-    try{
-           const users =  await User.find()
-            res.status(201).json(users);
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+  
+   //param pass from postman 
+    const query = req.query.new
 
-    }catch(err){
-        res.status(500).json(err)
-    }
+    try {
+
+    const users = query ? await User.find().sort({_id:-1}).limit(5) : await User.find();
+    // const users =  await User.find();
+
+    res.status(201).json(users);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/status",verifyTokenAndAdmin,async(req,res)=>{
+        const date = new Date()
+        const lastYear =new Date(date.setFullYear(date.getFullYear()-1))  
+
+        try{
+            const data = await User.aggregate([
+                {$match:{createdAt:{$gte: lastYear}}},
+                { $project:{    
+                            month:{$month:"$createdAt"}
+                        },
+                    },
+                    {
+                        $group:{
+                            _id:"$month",
+                            total:{$sum:1 }
+                        }
+                    }
+            ])
+            res.status(201).json(data)
+        }catch(err){
+            res.status(500).json(err)
+        }
 })
 
-module.exports = router 
+module.exports = router;
--- Routes/verifyToken.js
+++ Routes/verifyToken.js
@@ -3,43 +3,48 @@ const jwt = require("jsonwebtoken");
 const verifyToken = (req, res, next) => {
   const authHeader = req.headers;
 
-        const {token} = authHeader;   
- if(authHeader)
-    {
-       
+  const { token } = authHeader;
+
+  if (authHeader) {
     //  const token= authHeader.split(" ")[1]
-
-    jwt.verify(token, process.env.JWT_SEC, (err,user) => {
-        if (err) res.status(403).json("Token is Not Valid");
-        req.user = user;
-        next();
-      }
-     )
-    }
-    else{ res.status(401).json("You Are Not Authenticate User")
-        console.log(user)        
-}
-
+    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+      if (err) res.status(403).json("Token is Not Valid");
+      req.user = user;
+    
+      next();
+    });
+  } else {
+    res.status(401).json("You Are Not Authenticate User");
+    console.log(user);
+  }
 };
 
 const verifyTokenAndAuthorization = (req, res, next) => {
+ 
   verifyToken(req, res, () => {
-    if (req.user._id === req.params.id || req.user.usAdmin) {
-        next()
-    }else{
-        res.status(403).json("You Are Not Allow To ")
+ 
+    if (req.user.id === req.params.id || req.user.usAdmin) {
+     
+      next();
+    } else {
+      res.status(403).json("You Are Not Allow To Do That");
+    }
+  });
+  // verifyToken(req, res, next);
+};
+
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.usAdmin) {
+      next();
+    } else {
+      res.status(403).json("You Are Not Allow To ");
     }
   });
 };
 
-const verifyTokenAndAdmin = (req, res, next) => {
-    verifyToken(req, res, () => {
-      if (req.user.usAdmin) {
-          next()
-      }else{
-          res.status(403).json("You Are Not Allow To ")
-      }
-    });
-  };
-
-module.exports = { verifyToken,verifyTokenAndAuthorization,verifyTokenAndAdmin };
+module.exports = {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+};
